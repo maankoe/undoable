476b79fee0384d77be24ffbd22e6ab5b2ebb2bc0
maanrit <108694058+maanrit@users.noreply.github.com>
2022-07-04 20:26:59 +0100

    Written test to demonstrate concept

--- test/test_undoable.py
+++ test/test_undoable.py
@@ -1,8 +1,25 @@
 import unittest
 
-from undoable import Undoable
+from undoable import Undoable, undoable
+
+
+class Stateful(Undoable):
+    def __init__(self, x):
+    	super().__init__()
+    	self.x = x
+
+    @undoable('x')
+    def set_value(self, x):
+        self.x = x
 
 
 class TestUndoable(unittest.TestCase):
 	def test_something(self):
-		self.assertEqual(1, 1)
+		state = Stateful("Hello")
+		self.assertEqual("Hello", state.x)
+		state.set_value("world")
+		self.assertEqual("world", state.x)
+		state.undo()
+		self.assertEqual("Hello", state.x)
+		state.redo()
+		self.assertEqual("world", state.x)

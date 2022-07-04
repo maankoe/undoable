92c7dd59826d3999fcc84e6d981de2b8c714096b
maanrit <108694058+maanrit@users.noreply.github.com>
2022-07-04 20:28:35 +0100

    Added implementation via metaclass (experimenting)

--- src/undoable.py
+++ src/undoable.py
@@ -18,12 +18,13 @@ def undoable(*attr_names):
     return _undoable
 
 
-class Undoable:
-    def __init__(self):
-        self._undo_stack = []
-        self._redo_stack = []
+def undo(self):
+    _do_edit(self, self._undo_stack, self._redo_stack)
 
-    def _do_edit(self, pop_stack, push_stack):
+def redo(self):
+    _do_edit(self, self._redo_stack, self._undo_stack)
+
+def _do_edit(self, pop_stack, push_stack):
         if len(pop_stack) == 0:
             return
         edit = pop_stack.pop()
@@ -32,10 +33,31 @@ class Undoable:
         for k in edit:
             setattr(self, k, edit[k])
 
+
+
+class Undoable:
+    def __init__(self):
+        self._undo_stack = []
+        self._redo_stack = []
+
     def undo(self):
-        self._do_edit(self._undo_stack, self._redo_stack)
+        _do_edit(self, self._undo_stack, self._redo_stack)
 
     def redo(self):
-        self._do_edit(self._redo_stack, self._undo_stack)
+        _do_edit(self, self._redo_stack, self._undo_stack)
+        
+class UndoableMeta(type):
+    def __new__(meta, name, bases, attrs):
+        obj = type.__new__(meta, name, bases + (Undoable,), attrs)
+        obj._undo_stack = []
+        obj._redo_stack = []
+        obj.undo = undo
+        obj.redo = redo
+        return obj
+
+
+    
+
+    
 
 
--- test/test_undoable.py
+++ test/test_undoable.py
@@ -1,12 +1,30 @@
 import unittest
 
-from undoable import Undoable, undoable
+from undoable import *
+
+
+# class Stateful(Undoable):
+#     def __init__(self, x):
+#       super().__init__()
+#       self.x = x
+
+#     @undoable('x')
+#     def set_value(self, x):
+#         self.x = x
+
+class StatefulMeta(metaclass=UndoableMeta):
+    def __init__(self, x):
+        self.x = x
+
+    @undoable('x')
+    def set_value(self, x):
+        self.x = x
 
 
 class Stateful(Undoable):
     def __init__(self, x):
-    	super().__init__()
-    	self.x = x
+        super().__init__()
+        self.x = x
 
     @undoable('x')
     def set_value(self, x):
@@ -14,12 +32,22 @@ class Stateful(Undoable):
 
 
 class TestUndoable(unittest.TestCase):
-	def test_something(self):
-		state = Stateful("Hello")
-		self.assertEqual("Hello", state.x)
-		state.set_value("world")
-		self.assertEqual("world", state.x)
-		state.undo()
-		self.assertEqual("Hello", state.x)
-		state.redo()
-		self.assertEqual("world", state.x)
+    def test_undoable_meta(self):
+        state = StatefulMeta("Hello")
+        self.assertEqual("Hello", state.x)
+        state.set_value("world")
+        self.assertEqual("world", state.x)
+        state.undo()
+        self.assertEqual("Hello", state.x)
+        state.redo()
+        self.assertEqual("world", state.x)
+
+    def test_undoable(self):
+        state = Stateful("Hello")
+        self.assertEqual("Hello", state.x)
+        state.set_value("world")
+        self.assertEqual("world", state.x)
+        state.undo()
+        self.assertEqual("Hello", state.x)
+        state.redo()
+        self.assertEqual("world", state.x)
